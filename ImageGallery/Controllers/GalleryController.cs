ecd56a522cdabc037c8d7c8c77bebbc3d9f9a67f
courced <courced@rambler.ru>
2019-07-15 14:24:20 +0300

    after migration, start controllers

---
+++ ImageGallery/Controllers/GalleryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var model = new GalleryIndexModel()
+            {
+
+            };
+
+            return View(model);
+        }
+    }
+}
