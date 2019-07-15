c4f2d875884bf773bea9d8e5963887d5304fb675
courced <courced@rambler.ru>
2019-07-15 13:23:32 +0300

    before migration

---
+++ ImageGallery/Data/ImageGalleryDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ImageGallery.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Data
+{
+    public class ImageGalleryDbContext : DbContext
+    {
+        public ImageGalleryDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<ImageTag> ImageTags { get; set; }
+    }
+}
---
+++ ImageGallery/Models/GalleryImage.cs
@@ -0,0 +1,19 @@
+﻿using ImageGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageGallery.Data
+{
+    public class GalleryImage
+    {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public DateTime Created { get; set; }
+        public string Url { get; set; }
+
+        public virtual IEnumerable<ImageTag> Tags { get; set; }
+
+    }
+}
