813d7fb03ebd6ffae0067b39493cb958a3f535f6
courced <courced@rambler.ru>
2019-07-16 16:15:02 +0300

    test view

--- ImageGallery/Controllers/GalleryController.cs
+++ ImageGallery/Controllers/GalleryController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageGallery.Data;
+using ImageGallery.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +12,65 @@ namespace ImageGallery.Controllers
     {
         public IActionResult Index()
         {
+            List<ImageTag> testImageTags = new List<ImageTag>();
+            List<ImageTag> cityImageTags = new List<ImageTag>();
 
-            var model = new GalleryIndexModel()
+            ImageTag tag1 = new ImageTag()
             {
+                Description = "Adventure",
+                Id = 0
+            };
 
+            ImageTag tag2 = new ImageTag()
+            {
+                Description = "cool",
+                Id = 1
+            };
+
+            ImageTag tag3 = new ImageTag()
+            {
+                Description = "London",
+                Id = 2
+            };
+
+
+            testImageTags.AddRange(new List<ImageTag>{ tag1, tag2 });
+            cityImageTags.Add(tag3);
+
+
+            List<GalleryImage> imageList = new List<GalleryImage>()         
+            {
+                new GalleryImage()
+                {
+                    Title = "Test",
+                    Url = "https://files.adme.ru/files/news/part_70/704610/preview-26276115-650x341-98-1481879211.jpg",
+                    Created = DateTime.Now,
+                    Tags = testImageTags
+
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Test1",
+                    Url = "https://fullpicture.ru/wp-content/uploads/2016/02/traveling-dog1.jpg",
+                    Created = DateTime.Now,
+                    Tags = testImageTags
+
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Test2",
+                    Url = "https://st.depositphotos.com/2138251/3697/i/950/depositphotos_36972971-stock-photo-dog-in-the-boat.jpg",
+                    Created = DateTime.Now,
+                    Tags = cityImageTags
+
+                },
+            };
+
+            GalleryIndexModel model = new GalleryIndexModel()
+            {
+                Images = imageList
             };
 
             return View(model);
